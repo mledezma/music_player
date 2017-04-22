11fcd8bc2e425a3f6d93dd07485f1a34d8759f1b
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-04-22 11:56:26 -0600

    swiched some variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 (function(window){
     var audio = new Audio();
     var songs = '';
-    var thisSong = '';
+    var songList = [];
+    var thisSong = -1;
     var playerControls = document.getElementById('player-controls');
     var format = '';
+    var previous = '';
+    var next = '';
 
     // IIFE that calls the jason
     (function _requestSongs() {
@@ -15,6 +18,7 @@
                 if (httpRequest.status === 200) {
                     var response = JSON.parse(httpRequest.responseText);
                     songs = response.songs;
+                    _composeSongList();
                     _init();
                 } else {
                     alert('There was a problem with the request.');
@@ -23,6 +27,16 @@
         }       
     }());
 
+    // Compose songList object
+    function _composeSongList() {
+        console.log(songs);
+        console.log(songs.length);
+        for(var i = 0; i < 2; i++) {
+            songList.push(songs[i]);
+            console.log(songList)
+        }
+    }
+
     // Initialize the audio player
     function _init() {
         _createList();
@@ -30,14 +44,18 @@
         playerControls.appendChild(_timeline());  
         playerControls.appendChild(_volume()); 
         playerControls.appendChild(_songCurrentTime()[0]);           
-        playerControls.appendChild(_songCurrentTime()[1]);           
+        playerControls.appendChild(_songCurrentTime()[1]); 
+        playerControls.appendChild(_back()); 
+        playerControls.appendChild(_next());         
+        document.getElementById('initialTime').innerHTML = '0:00';
+        document.getElementById('totalTime').innerHTML = '0:00';          
                   
     }
 
     // Creates the list based in the json.
     function _createList() {
         var id = 0;        
-        songs.forEach(function(song) {
+        songList.forEach(function(song) {
             var tr = document.createElement('tr');
             tr.id = id++;
             for(var info in song) {
@@ -57,13 +75,14 @@
         i.style.fontSize ='44px';
         i.id = 'playBtn';
         i.addEventListener('click', function(event) {
-            if(thisSong !== '' && event.target.id === 'playBtn') {
+            console.log(thisSong)
+            if(thisSong !== -1 && event.target.id === 'playBtn') {
                 audio.play();
                 playBtn.className = '';
                 playBtn.classList.add('fa', 'fa-pause');   
                 playBtn.id = 'pauseBtn';               
             }
-            else if(thisSong !== '' && event.target.id === 'pauseBtn') {
+            else if(thisSong !== -1 && event.target.id === 'pauseBtn') {
                 audio.pause();
                 pauseBtn.id = 'playBtn'; 
                 playBtn.className = '';
@@ -80,23 +99,19 @@
     function _reset() {
         audio.pause();
         audio.currentTime = 0;
-        if(document.getElementById('pauseBtn')) pauseBtn.id = 'playBtn'; 
-        playBtn.className = '';
-        playBtn.classList.add('fa', 'fa-play');
-        
+        if(document.getElementById('pauseBtn')) pauseBtn.id = 'playBtn';
     }
 
     // Plays the song
     function _loadsong() {
         _reset();
-        _checkPlay()
+        _checkPlayType()
         var clickedSong = songs[thisSong.id]
         audio.src = clickedSong.name + format;
         playBtn.className = '';
         playBtn.classList.add('fa', 'fa-pause');   
         playBtn.id = 'pauseBtn';
         audio.play();
-        
     }
 
     // Adds a range that controls the position in the current time of the song
@@ -178,7 +193,7 @@
         return [initialTime, totalTime];
     }
 
-    // Switch to minutes
+    // Switch format to minutes
     function _toMinutes(time) {		
 		time /= 60;
 		time = time.toFixed(2);
@@ -187,7 +202,7 @@
 	}
 
     // Check type
-    function _checkPlay() {
+    function _checkPlayType() {
 		var canPlay = {
 			mp3: audio.canPlayType('audio/mp3'),
 			ogg: audio.canPlayType('audio/ogg'),
@@ -199,13 +214,66 @@
 		if(canPlay.wav !== '') return format = '.wav';
 	}
 
+    // Goes to the next song
+    function _next() {    
+        var i = document.createElement('i');
+        i.classList.add('fa', 'fa-forward');
+        i.setAttribute('aria-hidden', 'true');
+        i.style.fontSize ='44px';
+        i.addEventListener('click', function() {  
+            var nextSong = parseInt(thisSong.id)+1;
+            console.log(thisSong);
+            audio.src = songs[thisSong.id].name + format;
+            audio.play();
+            _elementCount('next'); 
+            console.log(thisSong);
+                                                       
+        })
+        return i;
+    }
+
+    // Goes to the last song
+    function _back() {     
+        var i = document.createElement('i');
+        i.classList.add('fa', 'fa-backward');
+        i.setAttribute('aria-hidden', 'true');
+        i.style.fontSize ='44px';
+        i.addEventListener('click', function(){
+            var nextSong = parseInt(thisSong.id)-1; 
+            console.log(previous);
+            audio.src = songs[thisSong.id].name + format;
+            audio.play();
+            _elementCount('previous');            
+        })
+        return i;
+    }
+
+    // Takes the next or the last element
+    function _elementCount(current) {
+        var songCount = document.getElementById('listSongs').getElementsByTagName('tr');
+        if(current == 'next') {
+            for(var i = 0; i < songCount.length; i++) {
+                if(songCount[i] == thisSong) {
+                    thisSong = songCount[i + 1];
+                    console.log(thisSong)
+                }
+            }
+        }
+        if(current == 'previous') {
+            for(var i = 0; i < songCount.length; i++) {
+                if(songCount[i] == thisSong) {
+                    thisSong = songCount[i - 1];
+                }
+            }
+        }
+    }
     window.Player = {
         currentSong: function(event){
             thisSong = event.target.parentNode;
         },
         loadSong: function(thisSong){
             loadSong(thisSong);
-        }
+        },
     }
 }(window));
 
