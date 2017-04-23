49f31f686b01fd399144c2a91d3653959505e898
Marco <marcoledezmacordero09@gmail.com>
2017-04-22 20:32:01 -0600

    Added and fixed
    Added almost all and fixed almost everything

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
     var songs = '';
     var songList = [];
     var thisSong = -1;
-    var playerControls = document.getElementById('player-controls');
     var format = '';
-    var previous = '';
-    var next = '';
+    var nextSong = '';
+    var previousSong = '';    
+    var errorStop = null;
+    var playerControls = document.getElementById('player-controls');    
 
     // IIFE that calls the jason
     (function _requestSongs() {
@@ -18,7 +19,6 @@
                 if (httpRequest.status === 200) {
                     var response = JSON.parse(httpRequest.responseText);
                     songs = response.songs;
-                    _composeSongList();
                     _init();
                 } else {
                     alert('There was a problem with the request.');
@@ -27,33 +27,34 @@
         }       
     }());
 
-    // Compose songList object
-    function _composeSongList() {
-        console.log(songs);
-        console.log(songs.length);
-        for(var i = 0; i < 2; i++) {
-            songList.push(songs[i]);
-            console.log(songList)
-        }
-    }
-
     // Initialize the audio player
     function _init() {
-        _createList();
+        _composeSongList();        
+        _renderList();
+        _checkPlayType();
         playerControls.appendChild(_play());    
         playerControls.appendChild(_timeline());  
         playerControls.appendChild(_volume()); 
         playerControls.appendChild(_songCurrentTime()[0]);           
-        playerControls.appendChild(_songCurrentTime()[1]); 
-        playerControls.appendChild(_back()); 
-        playerControls.appendChild(_next());         
+        playerControls.appendChild(_songCurrentTime()[1]);
         document.getElementById('initialTime').innerHTML = '0:00';
-        document.getElementById('totalTime').innerHTML = '0:00';          
-                  
+        document.getElementById('totalTime').innerHTML = '0:00';  
+        playerControls.appendChild(_back()); 
+        playerControls.appendChild(_next());  
+        _updateRange();    
+        songEnded();                 
+    }
+
+    /* Compose songList object
+    This the object for the display of the list */
+    function _composeSongList() {
+        for(var i = 0; i < 2; i++) {
+            songList.push([songs[i].name,songs[i].artist]);
+        }
     }
 
     // Creates the list based in the json.
-    function _createList() {
+    function _renderList() {
         var id = 0;        
         songList.forEach(function(song) {
             var tr = document.createElement('tr');
@@ -67,6 +68,23 @@
         });
     }
 
+    // Load the song and starts the functionality
+    function initSong(thisSong) {
+        _playSong();
+        _highlightSong(thisSong, true);         
+    }
+    
+    // Plays the song
+    function _playSong() {
+        _reset();
+        var clickedSong = songs[thisSong.id]
+        audio.src = clickedSong.src + format;
+        playBtn.className = '';
+        playBtn.classList.add('fa', 'fa-pause');   
+        playBtn.id = 'pauseBtn';
+        audio.play();
+    }
+
     // Initialize the play and the pause button and his function
     function _play() {
         var i = document.createElement('i');
@@ -75,7 +93,6 @@
         i.style.fontSize ='44px';
         i.id = 'playBtn';
         i.addEventListener('click', function(event) {
-            console.log(thisSong)
             if(thisSong !== -1 && event.target.id === 'playBtn') {
                 audio.play();
                 playBtn.className = '';
@@ -97,28 +114,14 @@
 
     // Resets the lists
     function _reset() {
-        audio.pause();
-        audio.currentTime = 0;
         if(document.getElementById('pauseBtn')) pauseBtn.id = 'playBtn';
-    }
-
-    // Plays the song
-    function _loadsong() {
-        _reset();
-        _checkPlayType()
-        var clickedSong = songs[thisSong.id]
-        audio.src = clickedSong.name + format;
-        playBtn.className = '';
-        playBtn.classList.add('fa', 'fa-pause');   
-        playBtn.id = 'pauseBtn';
-        audio.play();
     }
 
     // Adds a range that controls the position in the current time of the song
     function _timeline() {
         var range = document.createElement('input');
         range.setAttribute('type', 'range');
-		range.setAttribute('min', 0);
+		range.setAttribute('min', 0);    
 		range.setAttribute('style', 'width: 70%;');
         range.value = 0;
         range.id = 'timeRange';
@@ -160,26 +163,28 @@
         return range        
     }
 
-    // Highlight Song
-    function loadSong(thisSong) {
-        _loadsong();
-        _songCurrentTime();
-        _updateRange();
-        parentRow = thisSong.parentNode.className;
-        var tr = document.getElementsByTagName('tr');
+    // Highlight the actual song
+    function _highlightSong(thisSong, clicked) {
+        parentRow = thisSong.className;
+        var container = document.getElementById('listSongs');
+        var tr = container.getElementsByTagName('tr');
         if(thisSong.parentNode.parentNode.id == 'tHeader') {
             return false;
         }
         for(var i = 0; i < tr.length; i++) {
             if(tr[i].classList == 'selected') {
-                _reset();
+                console.log(tr[i])
                 tr[i].classList.remove("selected");
+                parentRow = thisSong.className;
             }
         }
 
-        if(parentRow != 'selected') {
+        if(parentRow != 'selected' && clicked == true) {
             thisSong.parentNode.className = 'selected';                  
-        }        
+        }
+        if(parentRow != 'selected' && clicked == false) {
+            thisSong.className = 'selected';                  
+        }
     }
 
     // Shows the duration and the current time of the song
@@ -194,7 +199,7 @@
     }
 
     // Switch format to minutes
-    function _toMinutes(time) {		
+    function _toMinutes(time) {	
 		time /= 60;
 		time = time.toFixed(2);
 		time += '';
@@ -215,19 +220,13 @@
 	}
 
     // Goes to the next song
-    function _next() {    
+   function _next() { 
         var i = document.createElement('i');
         i.classList.add('fa', 'fa-forward');
         i.setAttribute('aria-hidden', 'true');
         i.style.fontSize ='44px';
-        i.addEventListener('click', function() {  
-            var nextSong = parseInt(thisSong.id)+1;
-            console.log(thisSong);
-            audio.src = songs[thisSong.id].name + format;
-            audio.play();
-            _elementCount('next'); 
-            console.log(thisSong);
-                                                       
+        i.addEventListener('click', function() {
+            _elementCount('next');                                               
         })
         return i;
     }
@@ -239,10 +238,6 @@
         i.setAttribute('aria-hidden', 'true');
         i.style.fontSize ='44px';
         i.addEventListener('click', function(){
-            var nextSong = parseInt(thisSong.id)-1; 
-            console.log(previous);
-            audio.src = songs[thisSong.id].name + format;
-            audio.play();
             _elementCount('previous');            
         })
         return i;
@@ -250,34 +245,62 @@
 
     // Takes the next or the last element
     function _elementCount(current) {
-        var songCount = document.getElementById('listSongs').getElementsByTagName('tr');
         if(current == 'next') {
-            for(var i = 0; i < songCount.length; i++) {
-                if(songCount[i] == thisSong) {
-                    thisSong = songCount[i + 1];
-                    console.log(thisSong)
-                }
-            }
+            nextSong++;
+            _validSong();
+            if(errorStop) {
+                nextSong-- 
+                return
+            }           
+            audio.src = songs[nextSong].src + format;
+            audio.play();
+            thisSong = document.getElementById(nextSong);
+            _highlightSong(thisSong, false);         
         }
         if(current == 'previous') {
-            for(var i = 0; i < songCount.length; i++) {
-                if(songCount[i] == thisSong) {
-                    thisSong = songCount[i - 1];
-                }
-            }
+            nextSong--;
+            _validSong();
+            if(errorStop) {
+                nextSong++ 
+                return
+            }            
+            audio.src = songs[nextSong].src + format;
+            audio.play();
+            thisSong = document.getElementById(nextSong);            
+            _highlightSong(thisSong, false);         
         }
     }
+
+    // Validates the current song
+    function _validSong() {
+        errorStop = false;
+        console.log(errorStop);
+        if(nextSong === -1 || nextSong > songs.length-1) {
+            document.body.classList.add('song-error');
+            setTimeout(function() {
+                document.body.classList.remove('song-error');    
+            }, 1000)
+            errorStop = true;            
+        }
+    }
+
+    // Goes to the next song if exist wheh the song ends
+    function songEnded() {
+        audio.addEventListener('ended', function(){
+            _elementCount('next');
+        })
+    };
+
     window.Player = {
-        currentSong: function(event){
+        initPlayer: function(event){
             thisSong = event.target.parentNode;
-        },
-        loadSong: function(thisSong){
-            loadSong(thisSong);
-        },
+            nextSong = parseInt(thisSong.id);
+            previousSong = parseInt(thisSong.id);
+            initSong(event.target);
+        }
     }
 }(window));
 
 document.getElementById('listSongs').addEventListener('click', function(event) {
-    Player.currentSong(event);
-    Player.loadSong(event.target)
+    Player.initPlayer(event)
 });
